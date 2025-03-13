3188aa025863a4300d68f753f263d31bbf8f2505
Goran Suvacarev <goran.suvacarev.22@singimail.rs>
2025-03-13 20:06:58 +0100

    .

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatToolbar} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    MatToolbar
+    MatToolbar,
+    MatAnchor
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
